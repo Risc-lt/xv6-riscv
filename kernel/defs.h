5f9e64cd1a0e98e2de05968098be46cf2f64a359
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-03-08 15:56:48 +0800

    fix: add declaration

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,8 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void*           kalloc_n_deref(void* pa);
+void            krefpage(void *pa);
 
 // log.c
 void            initlog(int, struct superblock*);
