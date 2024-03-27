f878f5d7f57f388247f1e14aa119adfff3f07088
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-03-27 10:04:26 +0800

    fix: declaration in user.h

--- user/user.h
+++ user/user.h
@@ -23,8 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-void *mmap(int length, int prot, int flags, int fd);
-int munmap(void *addr, int length);
+void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+int munmap(void *addr, size_t length);
 
 // ulib.c
 int stat(const char*, struct stat*);
